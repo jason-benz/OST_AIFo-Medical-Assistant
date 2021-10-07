288c8515955497dd036d8065afaaf7b15ef004b2
Jason Benz <j.benz@webbenz.ch>
2021-10-07 22:18:58 +0200

    Basic hello world to chat bot

--- src/main/java/MedicalAssistant.java
+++ src/main/java/MedicalAssistant.java
@@ -1,7 +1,29 @@
+import java.io.IOException;
+
+import com.google.cloud.dialogflow.v2.*;
+
 public class MedicalAssistant {
-    public static void main (String[] args)
+    private static final String projectId = "medical-assistant-jgrt";
+    private static final String sessionId = "JavaClientSession";
+    private static final String text = "Hello World";
+    private static final String languageCode = "en-US";
+
+    public static void main (String[] args) throws IOException
     {
-        System.out.println("MedicalAssistant check");
-        System.out.println("Project ID: medical-assistant-jgrt");
+        try (SessionsClient sessionsClient = SessionsClient.create()) {
+            SessionName session = SessionName.of(projectId, sessionId);
+            System.out.println("Session Path: " + session.toString());
+
+            TextInput.Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
+            QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+
+            DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+
+            QueryResult queryResult = response.getQueryResult();
+            String intent = queryResult.getIntent().getDisplayName();
+
+            System.out.println(intent);
+            System.out.println(queryResult.getFulfillmentText());
+        }
     }
 }
