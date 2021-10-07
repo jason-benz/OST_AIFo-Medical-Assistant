db67a3fdf04dab4c5237035d64d0656a496fd5f1
Jason Benz <j.benz@webbenz.ch>
2021-10-07 22:03:52 +0200

    Added google key and project id

--- src/main/java/MedicalAssistant.java
+++ src/main/java/MedicalAssistant.java
@@ -2,5 +2,6 @@ public class MedicalAssistant {
     public static void main (String[] args)
     {
         System.out.println("MedicalAssistant check");
+        System.out.println("Project ID: medical-assistant-jgrt");
     }
 }
